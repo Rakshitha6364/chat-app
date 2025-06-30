b89409d0b97d30733bbc46ab67f1d90f20a54c83
Rakshitha Ck <76807894+Rakshitha6364@users.noreply.github.com>
2025-06-30 11:49:23 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,5 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5000"); // Make sure your backend is running on this port
+const socket = io("https://chat-app-2-efnv.onrender.com");
+
+// const socket = io("http://localhost:5000"); // Make sure your backend is running on this port
 export default socket;
